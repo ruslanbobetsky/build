655f4cc39aeb33d36efc6f45a4ee22e7493d5b03
ruslanbobetsky <ruslanbobetsky@gmail.com>
2021-07-21 14:58:37 +0300

    close almost

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@
 // 	new WOW().init();
 // });
 
+// $(document).ready(function(){
+// 	$("#about-us").on("click","a", function (event) {
+// 	  //отменяем стандартную обработку нажатия по ссылке
+// 	  event.preventDefault();
+// 	  //забираем идентификатор бока с атрибута href
+// 	  var id  = $(this).attr('href'),
+// 	  //узнаем высоту от начала страницы до блока на который ссылается якорь
+// 	  top = $(id).offset().top;
+// 	  //анимируем переход на расстояние - top за 1500 мс
+// 	  $('body,html').animate({scrollTop: top}, 1500);
+// 	});
+//   });
+
+
 function testWebP(callback) {
 	var webP = new Image();
 	webP.onload = webP.onerror = function () {
